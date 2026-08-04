b53b89f44876ed2c7e2e49a1508afea610232640
William Crane <wcran7@gmail.com>
2026-08-04 14:53:52 +1000

    feat(user-controller): relearn basic GET and POST commands; NestJS makes routing nice

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Query, Param } from '@nestjs/common';
+
+// @Get('all')     // GET /user/all
+// @Get('all')     // GET /user/:id - dynamic segment
+// @Post()         // POST /user
+// @Put(':id')     // PUT /user/:id
+// @Delete(':id')  // DELETE /user/:id
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  @Get()
+  getUsers(@Query('name') name: string) {
+    // ...
+  }
+
+  @Get(':id')
+  getUserById(@Param('id') id: string) {
+    // ...
+  }
+
+  @Post()
+  createUser(@Body() body: any) {
+    return {
+      message: 'User created successfully',
+    };
+  }
+}

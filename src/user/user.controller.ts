20f1a35c44bba645b5a035fc3b8cd8cfde3991df
William Crane <wcran7@gmail.com>
2026-08-05 14:19:12 +1000

    feat(user-controller): learnt about PUT / POST and creating DTO files to store basic user data

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,10 @@
-import { Controller, Get, Post, Body, Query, Param } from '@nestjs/common';
+import { Controller, Get, Post, Put, Body, Query, Param } from '@nestjs/common';
 
-// @Get('all')     // GET /user/all
-// @Get('all')     // GET /user/:id - dynamic segment
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+// @Get()          // GET /user
+// @Get(':id')     // GET /user/:id
 // @Post()         // POST /user
 // @Put(':id')     // PUT /user/:id
 // @Delete(':id')  // DELETE /user/:id
@@ -10,18 +13,34 @@ import { Controller, Get, Post, Body, Query, Param } from '@nestjs/common';
 export class UserController {
   @Get()
   getUsers(@Query('name') name: string) {
-    // ...
+    return {
+      name,
+    };
   }
 
   @Get(':id')
   getUserById(@Param('id') id: string) {
-    // ...
+    return {
+      id,
+    };
   }
 
   @Post()
-  createUser(@Body() body: any) {
+  createUser(@Body() createUserDto: CreateUserDto) {
     return {
+      data: createUserDto,
       message: 'User created successfully',
+    };
+  }
+
+  @Put(':id')
+  updateUser(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return {
+      data: {
+        id,
+        ...updateUserDto,
+      },
+      message: 'User updated successfully',
     };
   }
 }
